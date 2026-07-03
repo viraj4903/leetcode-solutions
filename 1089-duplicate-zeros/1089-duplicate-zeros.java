034744be67fa551302f9384059b944e5196a10a7
Viraj Tomar <virajx03@gmail.com>
2026-07-03 11:14:13 +0530

    Time: 15 ms (10.1%), Space: 47.1 MB (21.23%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                for(int j=(arr.length)-2;j>i;j--){
+                    arr[j+1]=arr[j];
+                }
+                if (i + 1 < arr.length) {
+                    arr[i + 1] = 0;
+                }
+                i++;
+            }
+        }
+    }
+}
